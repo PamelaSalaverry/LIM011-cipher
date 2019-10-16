3720ca54badf6661ad550c754223c8838cd74a00
PamelaSalaverry <pamela.salaverry.meza@gmail.com>
2019-10-16 01:00:48 -0500

    tercera version

--- src/cipher.js
+++ src/cipher.js
@@ -1,37 +1,37 @@
 window.cipher = {
-    encode: (offSet, string)=>{
-      let newMessage = '';
-      for (let i=0; i<string.length; i++){
-        let ascii = string.charCodeAt(i);
-        if (ascii >=65 && ascii <=90){
-          newMessage += String.fromCharCode ((ascii - 65 + parseInt(offSet)) % 26 + 65);
-        } else if (ascii>=97 && ascii<=122){ 
-          newMessage += String.fromCharCode ((ascii - 97 + parseInt(offSet)) % 26 + 97);
-        } else if (ascii>=48 && ascii<=57){
-          newMessage += String.fromCharCode ((ascii - 48 + parseInt(offSet)) % 10 + 48);
-        } else {
-          newMessage += string[i];
-        }
-        console.log(newMessage);
+  encode: (offSet, string)=>{
+    let newMessage = '';
+    for (let i=0; i<string.length; i++){
+      let ascii = string.charCodeAt(i);
+      if (ascii >=65 && ascii <=90){
+        newMessage += String.fromCharCode ((ascii - 65 + parseInt(offSet)) % 26 + 65);
+      } else if (ascii>=97 && ascii<=122){ 
+        newMessage += String.fromCharCode ((ascii - 97 + parseInt(offSet)) % 26 + 97);
+      } else if (ascii>=48 && ascii<=57){
+        newMessage += String.fromCharCode ((ascii - 48 + parseInt(offSet)) % 10 + 48);
+      } else {
+        newMessage += string[i];
       }
-      return newMessage;
-    },
-
-    decode: (offSet, string)=>{ 
-      let newMessage = '';
-      for (let i=0; i<string.length; i++){
-        let ascii = string.charCodeAt(i);
-        if (ascii >=65 && ascii <=90){
-          newMessage += String.fromCharCode (((ascii - 90 - parseInt(offSet)) % 26) + 90);
-        } else if (ascii>= 97 && ascii<= 122){
-          newMessage += String.fromCharCode ((122-(122 - ascii + parseInt(offSet)) % 26));
-        } else if (ascii>=48 && ascii<=57){
-          newMessage += String.fromCharCode ((57-(57 - ascii + parseInt(offSet)) % 10));
-        } else {
-          newMessage += string[i];
-        }
-        console.log(newMessage);
-      }
-      return newMessage;
+      console.log(newMessage);
     }
+    return newMessage;
+  },
+
+  decode: (offSet, string)=>{ 
+    let newMessage = '';
+    for (let i=0; i<string.length; i++){
+      let ascii = string.charCodeAt(i);
+      if (ascii >=65 && ascii <=90){
+        newMessage += String.fromCharCode (((ascii - 90 - parseInt(offSet)) % 26) + 90);
+      } else if (ascii>= 97 && ascii<= 122){
+        newMessage += String.fromCharCode ((122-(122 - ascii + parseInt(offSet)) % 26));
+      } else if (ascii>=48 && ascii<=57){
+        newMessage += String.fromCharCode ((57-(57 - ascii + parseInt(offSet)) % 10));
+      } else {
+        newMessage += string[i];
+      }
+      console.log(newMessage);
+    }
+    return newMessage;
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,43 +1,109 @@
 //Llamo a los botones y cajas de texto
-const ingresar = document.getElementById("inicio");
-const message = document.getElementById("message");
-const offset = document.getElementById ("offset");
-const bEncoder = document.getElementById("encoder");
-const bDecoder = document.getElementById("decoder");
-const limpiar = document.getElementById("limpiar");
-const codedMessage = document.getElementById("codedMessage");
-
-message.classList.add("hide");
-offset.classList.add("hide");
-bEncoder.classList.add("hide");
-bDecoder.classList.add("hide");
-limpiar.classList.add("hide");
-codedMessage.classList.add("hide");
-
-ingresar.addEventListener("click", () =>{
-
-	ingresar.classList.add("hide");
-	bEncoder.classList.remove("hide");
-	bDecoder.classList.remove("hide");
-	message.classList.remove("hide");
-	codedMessage.classList.remove("hide");
-	limpiar.classList.remove("hide");
-	offset.classList.remove("hide");
-
-	bEncoder.addEventListener("click", () =>{
-	let mensajeIngreso = document.getElementById("message").value;
-	let clave = document.getElementById("offset").value;
-	codedMessage.innerHTML = cipher.encode(clave, mensajeIngreso);
+const inicio = document.getElementById("inicio");
+const pantallaCodificar = document.getElementById("pantallaCodificar");
+const pantallaDecodificar = document.getElementById("pantallaDecodificar");
+const nombreVictima = document.getElementById("nombreVictima");
+const nombreAgresor = document.getElementById("nombreAgresor");
+const offsetCodificar = document.getElementById("offsetCodificar");
+const testimonio = document.getElementById("testimonio");
+const encoder = document.getElementById("encoder");
+const nombreVictimaCodificada = document.getElementById("nombreVictimaCodificada");
+const nombreAgresorCodificado = document.getElementById("nombreAgresorCodificado");
+const testimonioCodificado = document.getElementById("testimonioCodificado");
+const save = document.getElementById("save");
+const nombreVictimaCodificada_1 = document.getElementById("nombreVictimaCodificada-1");
+const nombreAgresorCodificado_1 = document.getElementById("nombreAgresorCodificado-1");
+const offsetDecodificar = document.getElementById("offsetDecodificar");
+const testimonioCodificado_1 = document.getElementById("testimonioCodificado-1");
+const nombreVictima_1 = document.getElementById("nombreVictima-1");
+const nombreAgresor_1 = document.getElementById("nombreAgresor-1");
+const testimonio_1 = document.getElementById("testimonio-1");
+const backHome = document.getElementById("backHome");
+const regresarHome = document.getElementById("regresarHome");
+const regresarCodificar = document.getElementById("regresarCodificar");
+const regresarHome_1 = document.getElementById("regresarHome-1");
+const regresarDecodificar = document.getElementById("regresarDecodificar");
+// Oculto pantallas
+document.getElementById("home").classList.add("hide");
+document.getElementById("functionEncode").classList.add("hide");
+document.getElementById("codedScreen").classList.add("hide");
+document.getElementById("functionDecode").classList.add("hide");
+document.getElementById("decodedScreen").classList.add("hide");
+// Llamo a funciones
+inicio.addEventListener("click", () =>{
+	// Oculto pantallas
+	document.getElementById("presentation").classList.add("hide");
+	document.getElementById("home").classList.remove("hide");
+	// Entro a la pantalla codificar
+	pantallaCodificar.addEventListener("click", ()=>{
+		// Oculto pantallas
+		document.getElementById("home").classList.add("hide");
+		document.getElementById("functionEncode").classList.remove("hide");
+		// Limpio valores
+		nombreVictima.value = "";
+		nombreAgresor.value ="";
+		testimonio.value ="";
+		// Creo primer boton retroceso
+		regresarHome.addEventListener("click", () => {
+			document.getElementById("functionEncode").classList.add("hide");
+			document.getElementById("home").classList.remove("hide")
+		})
+		// Llamo a la funcion codificar
+		encoder.addEventListener("click", () => {
+			let clave = offsetCodificar.value;
+			nombreVictimaCodificada.value = cipher.encode(clave, nombreVictima.value);
+			nombreAgresorCodificado.value = cipher.encode(clave, nombreAgresor.value);
+			testimonioCodificado.value = cipher.encode(clave, testimonio.value);
+			// Oculto pantallas
+			document.getElementById("functionEncode").classList.add("hide");
+			document.getElementById("codedScreen").classList.remove("hide");
+			// Archivo datos y vuelvo al inicio
+			save.addEventListener("click", () =>{
+				alert('Testimonio guardado')
+				document.getElementById("codedScreen").classList.add("hide");
+				document.getElementById("home").classList.remove("hide");
+			}),
+			// Creo segundo boton retroceso
+			regresarCodificar.addEventListener("click", () => {
+				document.getElementById("codedScreen").classList.add("hide");
+				document.getElementById("functionEncode").classList.remove("hide")
+			})
+		})
 	}),
-
-	bDecoder.addEventListener("click", () =>{
-	let mensajeSalida = document.getElementById("message").value;
-	let clave = document.getElementById("offset").value;
-	codedMessage.innerHTML= cipher.decode(clave, mensajeSalida);
-	}),
-
-	limpiar.addEventListener("click", () =>{
-	codedMessage.innerHTML = "";
+	// Entro a la pantalla decodificar
+	pantallaDecodificar.addEventListener("click", ()=>{
+		// Oculto pantallas
+		document.getElementById("home").classList.add("hide");
+		document.getElementById("functionDecode").classList.remove("hide")
+		// Limpio valores
+		nombreVictimaCodificada_1.value = "";
+		nombreAgresorCodificado_1.value ="";
+		testimonioCodificado_1.value ="";
+		// Creo tercer boton de retroceso
+		regresarHome_1.addEventListener("click", () => {
+			document.getElementById("functionDecode").classList.add("hide");
+			document.getElementById("home").classList.remove("hide")
+		})
+		// Llamo a la funcion decodificar
+		document.getElementById("decoder").addEventListener("click", ()=>{
+			let clave = offsetDecodificar.value;
+			nombreVictima_1.value = cipher.decode(clave, nombreVictimaCodificada_1.value);
+			nombreAgresor_1.value = cipher.decode(clave, nombreAgresorCodificado_1.value);
+			testimonio_1.value = cipher.decode(clave, testimonioCodificado_1.value);
+			// Oculto pantallas
+			document.getElementById("functionDecode").classList.add("hide");
+			document.getElementById("decodedScreen").classList.remove("hide")
+			// Creo cuarto boton de retroceso
+			regresarDecodificar.addEventListener("click", () =>{
+				document.getElementById("functionDecode").classList.remove("hide");
+				document.getElementById("decodedScreen").classList.add("hide")
+			})
+			// Vuelvo a inicio
+			backHome.addEventListener("click", () =>{
+				document.getElementById("decodedScreen").classList.add("hide");
+				document.getElementById("home").classList.remove("hide");
+			})
+		})
 	})
-
+	
 })
